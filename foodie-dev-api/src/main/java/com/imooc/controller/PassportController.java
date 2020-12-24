9b573c88888a2986009a7aff28226193723307d5
jfstylejf <15801850335@163.com>
2020-12-24 15:15:59 +0800

    5-2 add items into shopcart

--- foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -82,6 +82,9 @@ public class PassportController {
         CookieUtils.setCookie(request, response, "user",
                 JsonUtils.objectToJson(userResult), true);
 
+        // TODO 生成用户token，存入redis会话
+        // TODO 同步购物车数据
+
         return IMOOCJSONResult.ok();
     }
 
@@ -111,6 +114,9 @@ public class PassportController {
 
         CookieUtils.setCookie(request, response, "user",
                 JsonUtils.objectToJson(userResult), true);
+
+        // TODO 生成用户token，存入redis会话
+        // TODO 同步购物车数据
 
         return IMOOCJSONResult.ok(userResult);
     }
